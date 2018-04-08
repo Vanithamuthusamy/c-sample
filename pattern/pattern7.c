3cd896f3166d930cab981bd768de35eda20ff217
Vanithamuthusamy <34671388+Vanithamuthusamy@users.noreply.github.com>
2018-04-08 23:12:27 +0530

    Create pattern7.c

---
+++ pattern/pattern7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		
+		for(j=i;j>=1;j--)
+		{
+			printf("%d ",j);
+			
+		}
+		printf("\n");
+		
+	}
+	return 0;
+}
