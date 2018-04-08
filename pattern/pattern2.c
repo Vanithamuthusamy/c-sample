5dc052ece2fce23de1b6e3c8e2087c4c2a61fc73
Vanithamuthusamy <34671388+Vanithamuthusamy@users.noreply.github.com>
2018-04-08 22:54:45 +0530

    Create pattern2.c

---
+++ pattern/pattern2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=i;j<=5;j++)
+		{
+			printf("%d ",j);
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
