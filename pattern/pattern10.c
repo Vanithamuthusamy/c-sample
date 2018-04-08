c28f4cd316f2f4021c600e341434009da3aaf068
Vanithamuthusamy <34671388+Vanithamuthusamy@users.noreply.github.com>
2018-04-08 23:18:11 +0530

    Create pattern10.c

---
+++ pattern/pattern10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,j;
+	for(i=5;i>=1;i--)
+	{
+		
+		for(j=5;j>=i;j--)
+		{
+			printf("%d ",i);
+			
+		}
+		printf("\n");
+		
+	}
+	return 0;
+}
